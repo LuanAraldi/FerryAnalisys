6d79989442e9fa060a0c64016bfbfaad59c280ef
Araldi <luan.araldi@gmail.com>
2016-11-23 19:45:37 -0200

    First Blood :coffin:

---
+++ api/controllers/DadoFerryController.js
@@ -0,0 +1,11 @@
+/**
+ * DadosFerryController
+ *
+ * @description :: Server-side logic for managing Dadosferries
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
